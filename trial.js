fa3bc6f3995d80804283abc81b0517dc87effafa
Yash Gupta <anshya17@gmail.com>
2021-09-30 19:34:50 -0400

    remove package.json

---
+++ trial.js
@@ -0,0 +1,13 @@
+const searchBar = document.getElementById("searchbar");
+console.log(searchBar);
+// Example call to the Pokedex API
+/**fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+	.then(response => {
+		return response.json()
+	})
+	.then(responseAsJson => {
+		console.log(responseAsJson)
+	})
+	.catch(error => {
+		console.error(error)
+	})*/
