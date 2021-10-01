2c2afa63b08aeb1e54c1ab37c6e96381a9ad50f9
Yash Gupta <anshya17@gmail.com>
2021-09-30 20:00:22 -0400

    Made changes to trial.js

--- trial.js
+++ trial.js
@@ -1,5 +1,21 @@
 const searchBar = document.getElementById("searchbar");
 console.log(searchBar);
+const baseUrl = "https://pokeapi.co/api/v2/"
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    
+})
+
+const loadPokemon = async () => {
+    try {
+        const res = await fetch(baseUrl);
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
 // Example call to the Pokedex API
 /**fetch('https://pokeapi.co/api/v2/pokemon/ditto')
 	.then(response => {
